b2e6b27bfd7f64652636644f853d858393f9772e
Adriano <adricatena@gmail.com>
2023-01-24 08:25:49 -0300

    Initial commit

---
+++ components/Factura.jsx
@@ -0,0 +1,24 @@
+function Factura() {
+  return (
+    <section className="w-[20cm] p-3 border">
+      <header className="mb-1 text-center text-teal-500 border border-teal-500 rounded-md bg-teal-50">
+        <h2 className="font-bold">
+          COOPERATIVA DE AGUA POTABLE Y OTROS SERVICIOS PUBLICOS DE PARAJE LA
+          VIRGEN LTDA.
+        </h2>
+        <h3 className="font-bold ">
+          Paraje La Virgen - Dpto. Diamante - C.P. 3101 - Entre Ríos
+        </h3>
+        <p className="text-[10px]">
+          I.V.A. RRESPONSABLE INSCRIPTO - C.U.I.T.: 30-70834541-1 - Ing. Brutos:
+          Exento - INIC. DE ACTIVIDADES: 17/03/2003 Matrícula N° 24404
+        </p>
+      </header>
+      <article className="pl-2 text-teal-500 border border-teal-500 rounded-md bg-teal-50">
+        <p className="font-bold ">LIQUIDACIÓN DE SERVICIOS PÚBLICOS</p>
+      </article>
+    </section>
+  );
+}
+
+export default Factura;
